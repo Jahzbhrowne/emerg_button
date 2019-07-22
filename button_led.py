ffbb0fd969c3ca234409f96a42eeb2bcf4f05c4b
__I__am_OhenebaAduhene <34139302+OhenebaAduhene@users.noreply.github.com>
2019-07-22 16:15:02 +0100

    Add files via upload
    first upload

---
+++ button_led.py
@@ -0,0 +1,70 @@
+import os
+import time
+from gpiozero import LED, Button, Buzzer
+
+red = LED(17)
+green = LED(27)
+button = Button(22)
+bz = Buzzer(14)
+
+
+def startgreen():
+    print("Green light on")
+    green.on()
+    red.off()
+    
+def steadyred():
+    print("Steady red")
+    green.off()
+    red.on()
+    time.sleep(1)
+    
+    
+def buzzer_beep():
+    # Make the buzzer buzz on and off, half a second of
+    # sound followed by half a second of silence
+    print("Start walking")
+    count = 1
+    while count <= 4:
+        print("Beep")
+        bz.on()
+        time.sleep(0.5)
+        bz.off()
+        time.sleep(0.5)
+        count += 1
+        print(count)
+        
+        
+def flashingredgreen():
+    print("Flashing amber and green")
+    red.off()
+    green.off()
+
+    iCount = 1
+    while iCount <= 6:
+        red.on()
+        time.sleep(0.5)
+        red.off()
+        time.sleep(0.5)
+        iCount += 1
+    green.on()
+    
+    
+def flashingsequence():
+    print("Traffic Light sequence started")
+    # Green will already be on
+    #steadyamber()
+    steadyred()
+    buzzer_beep()
+    #dontwalk()
+    flashingredgreen()
+    #flashingamber()
+    startgreen()
+        
+def button_press():
+     button.wait_for_press()
+     return flashingsequence()
+    
+
+    
+        
