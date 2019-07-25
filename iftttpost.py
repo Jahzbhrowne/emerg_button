ac89960bd95909d846a612a8f9df6543c636c0a1
NancyNyantakyiwaa <52661239+NancyNyantakyiwaa@users.noreply.github.com>
2019-07-25 13:54:31 +0000

    Update iftttpost.py

--- iftttpost.py
+++ iftttpost.py
@@ -6,8 +6,8 @@ def ambulance():
     ambukey = "hMa7HtNAnbnNYmIaqMXhbNKNT3bTAYyemD6uHZveyEw"
     ambuevent = "nancy"
     ambu1 = geolocate.location
-    ambu2 = "this place"
-    ambu3 = "right now"
+    ambu2 = "val 1"
+    ambu3 = "val 2"
     
     url = "https://maker.ifttt.com/trigger/nancy/with/key/hMa7HtNAnbnNYmIaqMXhbNKNT3bTAYyemD6uHZveyEw"
     ambu = {"value1" : ambu1, "value2" :ambu2 , "value3" : ambu3}
@@ -18,8 +18,8 @@ def fire():
     firekey = "bO2KQdy-gXJhlkQLfYyP3P"
     fireevent = "button_pressed"
     fire1 = geolocate.location
-    fire2 = "val1"
-    fire3 = "val2"
+    fire2 = "val 1"
+    fire3 = "val 2"
     
     url = "https://maker.ifttt.com/trigger/button_pressed/with/key/bO2KQdy-gXJhlkQLfYyP3P"
     firemap = {"value1" : fire1, "value2" :fire2 , "value3" : fire3}
@@ -32,8 +32,8 @@ def police():
     key = "wU04UjKRQ7uw40h78c8vH"
     event = 'globalcodeproject'
     payload1 = geolocate.location
-    payload2 = 'val 2'
-    payload3 = 'val 3'
+    payload2 = 'val 1'
+    payload3 = 'val 2'
 
     url = 'https://maker.ifttt.com/trigger/globalcodeproject/with/key/wU04UjKRQ7uw40h78c8vH' 
     payload = {"value1" : payload1, "value2" : payload2, "value3" : payload3}
